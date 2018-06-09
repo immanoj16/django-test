acd06bc3ff4ffe5c4b103ac82c2a61d9baa56218
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-09 23:49:48 +0530

    Add: user endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView, DetailsView
+from .views import CreateView, DetailsView, UserView, UserDetailsView, UserRegistrationAPIView, UserProfileAPIView
 
 urlpatterns = {
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^expenses/$', CreateView.as_view(), name="create"),
     url(r'^expenses/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details"),
+    url(r'^users/$', UserView.as_view(), name="users"),
+    url(r'^users/(?P<pk>[0-9]+)/$', UserDetailsView.as_view(), name="user_details"),
+    url(r'^users/register/$', UserRegistrationAPIView.as_view(), name='register'),
+    url(r'^users/user-profile/$', UserProfileAPIView.as_view(), name='user_profile'),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
