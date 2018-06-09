284ad8cf87cbd0fa8da43aeaac612d910471ed91
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-09 23:36:26 +0530

    Add: expense views list and detail

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .serializers import ExpenseSerializer
+from .models import Expenses
+
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Expenses.objects.all()
+    serializer_class = ExpenseSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save(owner=self.request.user)
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Expenses.objects.all()
+    serializer_class = ExpenseSerializer
+    permission_classes = (permissions.IsAuthenticated, )
