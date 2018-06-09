86e8d4ec42701b7698eac68d1f2b3dea535ff568
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-09 23:38:37 +0530

    Add: endpoint urls for expense

--- django_test/urls.py
+++ django_test/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
