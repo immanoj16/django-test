a6c361819e0740013e63034f191f53aa89c94ab0
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-09 23:45:30 +0530

    Add: user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
+from django.contrib.sites.shortcuts import get_current_site
+from django.db.models import Q
+from django.conf import settings
 from rest_framework import serializers
 
-from .models import Expenses
+from .models import Expenses, UserProfile
 
 User = get_user_model()
 
@@ -64,3 +67,155 @@ class ExpenseSerializer(serializers.ModelSerializer):
         model = Expenses
         fields = ('id', 'name', 'owner', 'price', 'image', 'created')
         read_only_fields = ('created', )
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format for user registration"""
+
+    email = serializers.EmailField(
+        required=True,
+        label="Email Address"
+    )
+
+    password = serializers.CharField(
+        required=True,
+        label="Password",
+        style={'input_type': 'password'}
+    )
+
+    password_2 = serializers.CharField(
+        required=True,
+        label="Confirm Password",
+        style={'input_type': 'password'}
+    )
+
+    first_name = serializers.CharField(
+        required=True
+    )
+
+    last_name = serializers.CharField(
+        required=True
+    )
+
+    class Meta(object):
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password_2']
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
+
+    def validate_password(self, value):
+        if len(value) < getattr(settings, 'PASSWORD_MIN_LENGTH', 8):
+            raise serializers.ValidationError(
+                "Password should be atleast %s characters long." % getattr(settings, 'PASSWORD_MIN_LENGTH', 8)
+            )
+        return value
+
+    def validate_password_2(self, value):
+        data = self.get_initial()
+        password = data.get('password')
+        if password != value:
+            raise serializers.ValidationError("Passwords doesn't match.")
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
+
+    def create(self, validated_data):
+
+        user_data = {
+            'username': validated_data.get('username'),
+            'first_name': validated_data.get('first_name'),
+            'last_name': validated_data.get('last_name'),
+            'email': validated_data.get('email'),
+            'password': validated_data.get('password'),
+        }
+
+        UserProfile.objects.create_user_profile(
+            data=user_data,
+            is_active=True,
+            site=get_current_site(self.context['request'])
+        )
+
+        return validated_data
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format for user login"""
+
+    username = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        write_only=True,
+    )
+
+    email = serializers.EmailField(
+        required=False,
+        allow_blank=True,
+        write_only=True,
+        label="Email Address"
+    )
+
+    password = serializers.CharField(
+        required=True,
+        write_only=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta(object):
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self, data):
+        email = data.get('email', None)
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        if not email and not username:
+            raise serializers.ValidationError("Please enter username or email to login.")
+
+        user = User.objects.filter(
+            Q(email=email) | Q(username=username)
+        ).exclude(
+            email__isnull=True
+        ).exclude(
+            email__iexact=''
+        ).distinct()
+
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise serializers.ValidationError("This username/email is not valid.")
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise serializers.ValidationError("Invalid credentials.")
+
+        else:
+            raise serializers.ValidationError("User not active.")
+
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """A user serializer to aid in authentication and authorization."""
+
+    expenses = serializers.PrimaryKeyRelatedField(many=True, queryset=Expenses.objects.all())
+
+    class Meta:
+        """Map this serializer to the default django user model."""
+        model = User
+        fields = ('id', 'username', 'expenses')
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ['user']
