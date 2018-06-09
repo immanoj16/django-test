02514895458904a15c5c9145bfd51835bd4d6b97
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-09 23:29:11 +0530

    Add: expense model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+from django.conf import settings
+from django.db import models, transaction
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Expenses(models.Model):
+    """This class represents the expenses model."""
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    owner = models.ForeignKey('auth.User', related_name='expenselist', on_delete=models.CASCADE)
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='photos/')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
--- api/tests.py
+++ api/tests.py
@@ -1,25 +1,25 @@
 from django.test import TestCase
-from .models import Expenselist
+from .models import Expenses
 from django.contrib.auth.models import User
 
 
 class ModelTestCase(TestCase):
-    """This class defines the test suite for the expenselist model."""
+    """This class defines the test suite for the expenses model."""
 
     def setUp(self):
         """Define the test client and other test variables."""
         user = User.objects.create(username="nerd")
         self.name = "Sprite"
         # specify owner of a expenselist
-        self.expenselist = Expenselist(name=self.name, owner=user)
+        self.expenses = Expenses(name=self.name, owner=user)
 
     def test_model_can_create_a_expenselist(self):
-        """Test the expenselist model can create a bucketlist."""
-        old_count = Expenselist.objects.count()
-        self.expenselist.save()
-        new_count = Expenselist.objects.count()
+        """Test the expenses model can create a expense."""
+        old_count = Expenses.objects.count()
+        self.expenses.save()
+        new_count = Expenses.objects.count()
         self.assertNotEqual(old_count, new_count)
 
     def test_model_returns_readable_representation(self):
         """Test a readable string is returned for the model instance."""
-        self.assertEqual(str(self.expenselist), self.name)
+        self.assertEqual(str(self.expenses), self.name)
