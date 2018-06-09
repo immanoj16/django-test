12b216ee7e204b150684b4a35a10af8e6ee244c7
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-10 00:27:33 +0530

    Add: entry template

--- django_test/urls.py
+++ django_test/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
