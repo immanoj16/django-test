bc46c19f98cc6bbfc685df2032cc729bd45cd798
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-09 23:41:54 +0530

    Add: user models to extend the user model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,4 @@
-from django.db import models
-from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
-from django.dispatch import receiver
 from django.conf import settings
 from django.db import models, transaction
 from django.contrib.auth import get_user_model
@@ -20,3 +17,66 @@ class Expenses(models.Model):
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
+
+
+class UserProfileRegistrationManager(models.Manager):
+    """
+    Custom manager for ``UserProfile`` model.
+
+    The methods defined here provide shortcuts for user profile creation
+    and activation (including generation and emailing of activation
+    keys), and for cleaning out expired inactive accounts.
+    """
+
+    @transaction.atomic
+    def create_user_profile(self, data, is_active=False, site=None):
+        """
+        Create a new user and its associated ``UserProfile``.
+        Also, send user account activation (verification) email.
+
+        """
+
+        password = data.pop('password')
+        user = User(**data)
+        user.is_active = is_active
+        user.set_password(password)
+        user.save()
+
+        self.create_profile(user)
+
+        return user
+
+    def create_profile(self, user):
+        """
+        Create UserProfile for give user.
+        Returns created user profile on success.
+
+        """
+
+        self.create(
+            user=user
+        )
+
+
+class UserProfile(models.Model):
+    """
+    A model for user profile that also stores verification key.
+    Any methods under User will reside here.
+
+    """
+
+    ACTIVATED = "ALREADY ACTIVATED"
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    objects = UserProfileRegistrationManager()
+
+    class Meta:
+        verbose_name = u'user profile'
+        verbose_name_plural = u'user profiles'
+
+    def __str__(self):
+        return str(self.user)
