ab6b682acec7c34a2ff53385d36f418a9a1fad3a
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-10 09:06:15 +0530

    Add: error reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk';
 
 import authReducer from './authReducer';
 import expenseReducer from './expenseReducer';
+import errorReducer from './errorReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  expenses: expenseReducer
+  expenses: expenseReducer,
+  error: errorReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
