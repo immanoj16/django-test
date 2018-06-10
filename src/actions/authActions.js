8d20118a7232674fc22f6e0cb1b65e55ac6ebe22
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-10 08:30:34 +0530

    Add: auth action creators

---
+++ src/actions/authActions.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
+
+export const registerUser = (user, history) => dispatch => {
+  axios.post('/api/users/register/', user)
+    .then(res => history.push('/login'))
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    });
+};
+
+export const loginUser = user => dispatch => {
+  axios.post('/api/users/login/', user)
+    .then(res => {
+      // save to local storage
+      const { token } = res.data;
+      const jwtToken = `JWT ${token}`;
+      // set token to local storage
+      localStorage.setItem('jwtToken', jwtToken);
+      // set token to Auth header
+      setAuthToken(jwtToken);
+      // decode the jwt token
+      const decoded = jwt_decode(localStorage.jwtToken);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response
+      })
+    })
+
+  /*$.ajax({
+    type: 'POST',
+    url: '/api/obtain-auth-token/',
+    data: user,
+    success: function(res){
+      console.log(res)
+    }
+  })*/
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+};
+
+export const logoutUser = () => dispatch => {
+  // Remove the token from local storage
+  localStorage.removeItem('jwtToken');
+  // Remove auth token for future use
+  setAuthToken(false);
+  // Remove user from store
+  dispatch(setCurrentUser({}));
+};
